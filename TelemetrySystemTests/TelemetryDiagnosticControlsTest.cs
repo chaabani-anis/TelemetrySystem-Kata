5261c7219c38f1a2888f2f1e453b34a82118c5fd
Anis <chaabani.anis@gmail.com>
2019-04-06 21:07:25 +0200

    Initial commit

---
+++ TelemetrySystemTests/TelemetryDiagnosticControlsTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TelemetrySystemTests
+{
+    [TestClass]
+    public class TelemetryDiagnosticControlsTest
+    {
+        [TestMethod]
+        public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+        {
+            
+        }
+    }
+}
